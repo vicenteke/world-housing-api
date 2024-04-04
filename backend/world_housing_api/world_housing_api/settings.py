fdab93f209806aad704f3b84f03f843b285c8280
Vicente Knihs Erbs <vicenteknihs@gmail.com>
2024-04-04 10:30:59 -0300

    feat: include CORS policy on backend

--- backend/world_housing_api/world_housing_api/settings.py
+++ backend/world_housing_api/world_housing_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
     'brazil',
 ]
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS
+ALLOWED_HOSTS=['*']
+CORS_ALLOW_ALL_ORIGINS = True
