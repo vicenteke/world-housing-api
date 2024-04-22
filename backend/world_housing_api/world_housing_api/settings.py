aba25f6e6149fa28086c6518ab8f6f9a7ca27ff9
Vicente Knihs Erbs <vicenteknihs@gmail.com>
2024-04-22 15:57:40 -0300

    fix(back-end): use env file for keys

--- backend/world_housing_api/world_housing_api/settings.py
+++ backend/world_housing_api/world_housing_api/settings.py
@@ -10,20 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-002nu+5mcq&41ce6(j5e@sqjz4tsvw-^y_8jbdzzyicc5-hk_='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
